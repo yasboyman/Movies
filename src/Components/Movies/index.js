334f9f47d21f2800fb50972dc0e69ef9ea015af7
yasir <yasiralabadi@hotmail.com>
2021-04-14 17:45:41 +0100

    css changes, pop up transition for  overview desc

--- src/Components/Movies/index.js
+++ src/Components/Movies/index.js
@@ -11,21 +11,19 @@ const Movie = ({key, movie, movieId, title, overview, releaseDate, posterPath, a
 
 
     return (
-        <div className='movies_container'>
+
 
             <div className={'movie'}>
                 <img src={`${image_API}${posterPath}`} alt={'title'}  />
 
                 <div className='move-info'>
                     <h3> {title} </h3>
-                    {/*<h4> {overview} </h4>*/}
-                    {/*<h4> Release Date: {releaseDate} </h4>*/}
                 </div>
-            </div>
+                <div className='move-desc-popup'>
+                    <h2>Overview:</h2>
+                    <p> {overview} </p>
 
-
-
-
+                </div>
 
         </div>
         )
