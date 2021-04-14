ffe88ddb2d8c1594112396acddce6e3504015841
yasir <yasiralabadi@hotmail.com>
2021-04-14 14:51:44 +0100

    initial api data fetched and sending to movie component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,62 @@
+import React, { useState, useEffect } from 'react';
 import logo from './assets/movie_logo.svg';
 import './App.css';
+import axios from "axios";
+import Movie from './Components/Movies/index'
 
-function App() {
+const App = () => {
+
+     const [movies, setMovies] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    const apiKey =  '547b4693cebd0509a71cadc54d008d4f'
+
+
+    const fetchData = async (path, callback) => {
+        try{
+            const response = await axios.get(`https://api.themoviedb.org/3/movie/${path}?api_key=${apiKey} `)
+            callback(response.data.results)
+        } catch (e) {
+            console.log('error', e)
+        }
+    }
+
+    useEffect(() => {
+        Promise.all([
+            fetchData('top_rated', setMovies),
+
+
+        ]).then(() => {
+            setLoading(false)
+        })
+    }, [])
+
+console.log(movies)
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
+
       </header>
+            <div>
+                {movies && movies.map((movie) => (
+                     <Movie
+                         key={movie.id}
+                         movie ={movie}
+                         id={movie.id}
+                         title={movie.title}
+                         overview={movie.overview}
+                         releaseDate={movie.release_date}
 
 
 
+                     />
+                ))}
+            </div>
 
+        {loading && 'Loading...'}
     </div>
   );
 }
---
+++ src/Components/Movies/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const Movie = () => {
+
+
+
+    return (
+        <div  className='movies'>
+
+            <h3>This is a movie component </h3>
+        </div>
+        )
+
+}
+
+    export default Movie
