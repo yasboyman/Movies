1f3bb3e9fae38c431fc43863dc5c9eea8e9dd3b4
yasir <yasiralabadi@hotmail.com>
2021-04-18 13:57:33 +0100

    link to imdb info added

--- src/Components/CastInfo/index.js
+++ src/Components/CastInfo/index.js
@@ -1,31 +1,37 @@
-import React, {useState,useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from "axios";
 import './CastInfo.css'
 
 
-const CastInfo = ({ apiKey, actorName, currentCastID ,profilePath}) => {
+const CastInfo = ({apiKey, actorName, currentCastID, profilePath}) => {
 
-    const[ castInfo, setCastInfo] = useState([])
+    const [castInfo, setCastInfo] = useState([])
 
-
-    useEffect( () => {
+    useEffect(() => {
         actorName && axios.get(`https://api.themoviedb.org/3/person/${currentCastID}?api_key=${apiKey}`)
             .then((response) => {
                 setCastInfo(response.data)
             }, (error) => {
                 console.log(error);
             })
-    },[])
+    }, [])
 
-     const image_API = 'https://image.tmdb.org/t/p/w200';
+    const image_API = 'https://image.tmdb.org/t/p/w200';
 
-    return(
+    const handleIMBClick = () => {
+        castInfo.imdb_id &&  window.open(`https://www.imdb.com/name/${castInfo.imdb_id}`)
+    }
+
+    console.log(castInfo)
+    return (
         <div className='actor_container'>
             <h2>{actorName}</h2>
-            {profilePath ?  <img src={`${image_API}${profilePath}`} alt={'title'}  /> : <div>No image</div>}
-            {    castInfo.birthday && <h5>  {castInfo.birthday}</h5> }
-            {    castInfo.place_of_birth && <h5>  {castInfo.place_of_birth}</h5> }
-            {    castInfo.biography && <h5 className='cast_biography'>  {castInfo.biography}</h5> }
+            {profilePath ? <img src={`${image_API}${profilePath}`} alt={'title'}/> : <div>No image</div>}
+            {castInfo.birthday && <h5>  {castInfo.birthday}</h5>}
+            {castInfo.place_of_birth && <h5>  {castInfo.place_of_birth}</h5>}
+            {castInfo.biography && <h5 className='cast_biography'>  {castInfo.biography}</h5>}
+             <h5 style={{color: 'lightblue'}} onClick={handleIMBClick}> Link to IMDB</h5>
+
         </div>
     )
 }
--- src/Components/Characters/index.js
+++ src/Components/Characters/index.js
@@ -1,27 +1,26 @@
-import React,{useState,} from 'react';
+import React, {useState,} from 'react';
 import './characters.css';
 import CastInfo from "../CastInfo";
 
 const Characters = ({cast, apiKey}) => {
 
-    const [currentCastInfo, setCurrentCastInfo]= useState('')
+    const [currentCastInfo, setCurrentCastInfo] = useState('')
 
     if (!cast) return null;
 
-    const cuttingArray = cast.cast && cast.cast.slice(0,6)
-    console.log('characters', cast && cast)
+    const cuttingArray = cast.cast && cast.cast.slice(0, 6)
 
     return (
         <div className='characters_container'>
-            {cast.cast && cuttingArray.map( actor => {
-                return ( <div className='character_info'>
-                       <CastInfo
-                       actorName={actor.name}
-                       actorID={actor.id}
-                       apiKey={apiKey}
-                       currentCastID={actor.id}
-                       profilePath={actor.profile_path}
-                       />
+            {cast.cast && cuttingArray.map(actor => {
+                return (<div className='character_info'>
+                        <CastInfo
+                            actorName={actor.name}
+                            actorID={actor.id}
+                            apiKey={apiKey}
+                            currentCastID={actor.id}
+                            profilePath={actor.profile_path}
+                        />
                     </div>
                 )
             })
